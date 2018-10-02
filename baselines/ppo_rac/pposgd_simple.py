f704ed20e491154cda8e548858f121cb094fb09c
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-02 14:29:24 +1300

    1. Revise the ppo_rac 2. Now working perfectly.

--- baselines/ppo_rac/pposgd_simple.py
+++ baselines/ppo_rac/pposgd_simple.py
@@ -8,6 +8,7 @@ from baselines.common.mpi_moments import mpi_moments
 from mpi4py import MPI
 from collections import deque
 
+import itertools
 
 def traj_segment_generator(pi, env, horizon,
                                      vf_lossandgrad,
@@ -223,7 +224,7 @@ def learn(env, policy_fn, *,
                                      pol_adam,
                                      compute_v_pred,
                                      gamma,
-                                     cur_lrmult * optim_stepsize * 0.1,
+                                     cur_lrmult * optim_stepsize * 10,
                                      optim_stepsize,
                                      stochastic=True)
 
@@ -238,6 +239,7 @@ def learn(env, policy_fn, *,
 
     assert sum([max_iters>0, max_timesteps>0, max_episodes>0, max_seconds>0])==1, "Only one time constraint permitted"
 
+    seg = None
     while True:
         if callback: callback(locals(), globals())
         if max_timesteps and timesteps_so_far >= max_timesteps:
@@ -257,14 +259,81 @@ def learn(env, policy_fn, *,
 
         logger.log("********** Iteration %i ************"%iters_so_far)
 
-        seg = seg_gen.__next__()
-        lrlocal = (seg["ep_lens"], seg["ep_rets"]) # local values
-        listoflrpairs = MPI.COMM_WORLD.allgather(lrlocal) # list of tuples
-        lens, rews = map(flatten_lists, zip(*listoflrpairs))
-        lenbuffer.extend(lens)
-        rewbuffer.extend(rews)
-        # if iters_so_far == 0:
-        #     result_record()
+        t = 0
+        ac = env.action_space.sample() # not used, just so we have the datatype
+        new = True # marks if we're on first timestep of an episode
+        ob = env.reset()
+
+        cur_ep_ret = 0 # return in current episode
+        cur_ep_len = 0 # len of current episode
+        ep_rets = [] # returns of completed episodes in this segment
+        ep_lens = [] # lengths of ...
+        horizon = timesteps_per_actorbatch
+
+        # Initialize history arrays
+        obs = np.array([ob for _ in range(horizon)])
+        rews = np.zeros(horizon, 'float32')
+        vpreds = np.zeros(horizon, 'float32')
+        news = np.zeros(horizon, 'int32')
+        acs = np.array([ac for _ in range(horizon)])
+        prevacs = acs.copy()
+
+        rac_alpha = optim_stepsize * cur_lrmult
+        rac_beta = optim_stepsize * cur_lrmult * 0.1
+
+        for t in itertools.count():
+            if timesteps_so_far % 10000 == 0 and timesteps_so_far > 0:
+                result_record()
+            prevac = ac
+            ac, vpred = pi.act(stochastic=True, ob=ob)
+            # Slight weirdness here because we need value function at time T
+            # before returning segment [0, T-1] so we get the correct
+            # terminal value
+            if t > 0 and t % horizon == 0:
+                seg = {"ob" : obs, "rew" : rews, "vpred" : vpreds, "new" : news,
+                    "ac" : acs, "prevac" : prevacs, "nextvpred": vpred * (1 - new),
+                    "ep_rets" : ep_rets, "ep_lens" : ep_lens}
+                ep_rets = []
+                ep_lens = []
+                break
+            i = t % horizon
+            obs[i] = ob
+            vpreds[i] = vpred
+            news[i] = new
+            acs[i] = ac
+            prevacs[i] = prevac
+            if env.spec._env_name == "LunarLanderContinuous":
+                ac = np.clip(ac, -1.0, 1.0)
+            next_ob, rew, new, _ = env.step(ac)
+            # Compute v target and TD
+            v_target = rew + gamma * np.array(compute_v_pred(next_ob.reshape((1, ob.shape[0]))))
+            adv = v_target - np.array(compute_v_pred(ob.reshape((1, ob.shape[0]))))
+
+            # Update V and Update Policy
+            vf_loss, vf_g = vf_lossandgrad(ob.reshape((1, ob.shape[0])), v_target,
+                                           rac_alpha)
+            vf_adam.update(vf_g, rac_alpha)
+            pol_loss, pol_g = pol_lossandgrad(ob.reshape((1, ob.shape[0])), ac.reshape((1, ac.shape[0])), adv,
+                                              rac_beta)
+            pol_adam.update(pol_g, rac_beta)
+
+            rews[i] = rew
+
+            cur_ep_ret += rew
+            cur_ep_len += 1
+            timesteps_so_far+=1
+            ob = next_ob
+            if new:
+                print(
+                    "Episode {} - Total reward = {}, Total Steps = {}".format(episodes_so_far, cur_ep_ret, cur_ep_len))
+                ep_rets.append(cur_ep_ret)
+                ep_lens.append(cur_ep_len)
+                rewbuffer.extend(ep_rets)
+                lenbuffer.extend(ep_lens)
+                cur_ep_ret = 0
+                cur_ep_len = 0
+                ob = env.reset()
+            t += 1
 
         add_vtarg_and_adv(seg, gamma, lam)
 
--- baselines/rac/rac_simple.py
+++ baselines/rac/rac_simple.py
@@ -11,65 +11,6 @@ import itertools
 import collections
 
 
-def traj_segment_generator(pi, env, horizon, stochastic):
-    global timesteps_so_far
-    t = 0
-    ac = env.action_space.sample()  # not used, just so we have the datatype
-    new = True  # marks if we're on first timestep of an episode
-    ob = env.reset()
-
-    cur_ep_ret = 0  # return in current episode
-    cur_ep_len = 0  # len of current episode
-    ep_rets = []  # returns of completed episodes in this segment
-    ep_lens = []  # lengths of ...
-
-    # Initialize history arrays
-    obs = np.array([ob for _ in range(horizon)])
-    rews = np.zeros(horizon, 'float32')
-    vpreds = np.zeros(horizon, 'float32')
-    news = np.zeros(horizon, 'int32')
-    acs = np.array([ac for _ in range(horizon)])
-    prevacs = acs.copy()
-
-    while True:
-        if timesteps_so_far % 10000 == 0 and timesteps_so_far > 0:
-            result_record()
-        prevac = ac
-        ac, vpred = pi.act(stochastic, ob)
-        # Slight weirdness here because we need value function at time T
-        # before returning segment [0, T-1] so we get the correct
-        # terminal value
-        if t > 0 and t % horizon == 0:
-            yield {"ob": obs, "rew": rews, "vpred": vpreds, "new": news,
-                   "ac": acs, "prevac": prevacs, "nextvpred": vpred * (1 - new),
-                   "ep_rets": ep_rets, "ep_lens": ep_lens}
-            # Be careful!!! if you change the downstream algorithm to aggregate
-            # several of these batches, then be sure to do a deepcopy
-            ep_rets = []
-            ep_lens = []
-        i = t % horizon
-        obs[i] = ob
-        vpreds[i] = vpred
-        news[i] = new
-        acs[i] = ac
-        prevacs[i] = prevac
-        if env.spec._env_name == "LunarLanderContinuous":
-            ac = np.clip(ac, -1.0, 1.0)
-        ob, rew, new, _ = env.step(ac)
-        rews[i] = rew
-
-        cur_ep_ret += rew
-        cur_ep_len += 1
-        timesteps_so_far += 1
-        if new:
-            ep_rets.append(cur_ep_ret)
-            ep_lens.append(cur_ep_len)
-            cur_ep_ret = 0
-            cur_ep_len = 0
-            ob = env.reset()
-        t += 1
-
-
 def result_record():
     global lenbuffer, rewbuffer, iters_so_far, timesteps_so_far, \
         episodes_so_far, tstart
@@ -143,10 +84,17 @@ def learn(env, policy_fn, *,
     vf_loss_names = ["vf_loss"]
 
     var_list = pi.get_trainable_variables()
+    # vf_var_list = [v for v in var_list if v.name.split("/")[1].startswith(
+    #     "vf")]
+    # pol_var_list = [v for v in var_list if v.name.split("/")[1].startswith(
+    #     "pol")]
+
     vf_var_list = [v for v in var_list if v.name.split("/")[1].startswith(
-        "vf")]
+        "vf") and v.name.split("/")[2].startswith(
+        "final")]
     pol_var_list = [v for v in var_list if v.name.split("/")[1].startswith(
-        "pol")]
+        "pol") and v.name.split("/")[2].startswith(
+        "final")]
 
     # Train V function
     vf_lossandgrad = U.function([ob, td_v_target, lrmult],
