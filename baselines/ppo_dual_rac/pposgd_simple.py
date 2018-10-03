73c7b68dd1d8159cf8b6a872f880c25a41db6b80
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-03 14:44:16 +1300

    For ppo_dual_rac 1. Add log to simple ctrl 2. Change the setting to be identical to PPO 3. Remove redundant logging

--- baselines/ppo_dual_rac/pposgd_simple.py
+++ baselines/ppo_dual_rac/pposgd_simple.py
@@ -354,8 +354,8 @@ def learn(env, policy_fn, *,
                 pol_adam.update(coef*sum_weighted_pol_gradients, rac_beta)
                 pol_gradients = []
                 t_0 = t
-                print(
-                    "Episode {} - Total reward = {}, Total Steps = {}".format(episodes_so_far, cur_ep_ret, cur_ep_len))
+                # print(
+                #     "Episode {} - Total reward = {}, Total Steps = {}".format(episodes_so_far, cur_ep_ret, cur_ep_len))
                 ep_rets.append(cur_ep_ret)
                 ep_lens.append(cur_ep_len)
                 rewbuffer.extend(ep_rets)
