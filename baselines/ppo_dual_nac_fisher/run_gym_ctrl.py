1d95ab986e3efa99febea81f0dc04469950562fb
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-03 14:45:03 +1300

    For ppo_dual_nac_fisher 1. Add log to simple ctrl 2. Change the setting to be identical to PPO 3. Remove redundant logging

--- baselines/ppo_dual_nac_fisher/run_gym_ctrl.py
+++ baselines/ppo_dual_nac_fisher/run_gym_ctrl.py
@@ -13,7 +13,7 @@ from baselines.common import tf_util as U
 from baselines import logger
 
 def train(env_id, num_timesteps, seed):
-    from baselines.ppo1 import mlp_policy, pposgd_simple
+    from baselines.ppo_dual_nac_fisher import mlp_policy, pposgd_simple
     U.make_session(num_cpu=1).__enter__()
     def policy_fn(name, ob_space, ac_space):
         return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
@@ -23,14 +23,16 @@ def train(env_id, num_timesteps, seed):
             max_timesteps=num_timesteps,
             timesteps_per_actorbatch=2048,
             clip_param=0.2, entcoeff=0.0,
-            optim_epochs=5, optim_stepsize=3e-4, optim_batchsize=64,
+            optim_epochs=10, optim_stepsize=1e-4, optim_batchsize=64,
             gamma=0.99, lam=0.95, schedule='linear'
         )
     env.close()
 
 def main():
     args = gym_ctrl_arg_parser().parse_args()
-    logger.configure(format_strs=['stdout', 'log', 'csv'], log_suffix = "PPO-"+args.env)
+    logger.configure(
+        format_strs = ['stdout', 'log', 'csv'], log_suffix = "PPO_Dual_NAC_Fisher-" + args.env)
+    logger.log("Algorithm: PPO_Dual_NAC_Fisher-" + args.env)
     train(args.env, num_timesteps=args.num_timesteps, seed=args.seed)
 
 if __name__ == '__main__':
