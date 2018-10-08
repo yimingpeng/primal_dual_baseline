714b57d4f4763ddd0efe21546ed3a309e59a13f6
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-08 16:30:27 +1300

    1. Add all-in-one run for hpc grid 2. Add run.sh for hpc scripts

--- baselines/common/generate_hpc_scripts.py
+++ baselines/common/generate_hpc_scripts.py
@@ -14,7 +14,6 @@ __status__ = "Prototype"
 # Scripts for generating GCP startup scripts
 import os
 
-
 f = open("../../hpc_scripts/template.sl")
 algorithms = ["ppo", "ppo_rac", "ppo_nac_fisher",
               "ppo_nac_advantage_fisher", "ppo_nac_advantage",
@@ -36,15 +35,15 @@ for algorithm in algorithms:
             # if 'source activate cmaes_baselines' in line and algorithm == "DDPG":
             #         line = line.replace("cmaes_baselines", "ddpg_baselines")
             if '--job-name=ddpg_Walker2D' in line:
-                line = line.replace("--job-name=ddpg_Walker2D", "--job-name="+algorithm+"_"+problem)
+                line = line.replace("--job-name=ddpg_Walker2D", "--job-name=" + algorithm + "_" + problem)
             if "/nesi/project/nesi00272/primal_dual_baseline/baselines/ddpg/" in line:
-                line = line.replace("ddpg",algorithm.lower())
+                line = line.replace("ddpg", algorithm.lower())
             if '--error=ddpg_InvertedPendulum.err' in line:
                 line = line.replace("--error=ddpg_InvertedPendulum",
-                                    "--error="+algorithm+"_"+problem+"_")
+                                    "--error=" + algorithm + "_" + problem + "_")
             if '--output=ddpg_InvertedPendulum_%A_%a.out' in line:
                 line = line.replace("--error=ddpg_InvertedPendulum_%A_%a",
-                                    "--error="+algorithm.lower()+"_"+problem+"_")
+                                    "--error=" + algorithm.lower() + "_" + problem + "_")
             if "Walker2DBulletEnv-v0" in line:
                 if algorithm == "DDPG":
                     line = "srun python main.py --env-id " + problem + "BulletEnv-v0" + " --seed $SLURM_ARRAY_TASK_ID\n"
@@ -71,9 +70,9 @@ for algorithm in algorithms:
                   problem + ".sl", 'w')
         for line in f:
             if '--job-name=ddpg_Walker2D' in line:
-                line = line.replace("--job-name=ddpg_Walker2D", "--job-name="+algorithm+"_"+problem)
+                line = line.replace("--job-name=ddpg_Walker2D", "--job-name=" + algorithm + "_" + problem)
             if "/nesi/project/nesi00272/primal_dual_baseline/baselines/ddpg/" in line:
-                line = line.replace("ddpg",algorithm.lower())
+                line = line.replace("ddpg", algorithm.lower())
             if "Walker2DBulletEnv-v0" in line:
                 if algorithm == "DDPG":
                     line = "python main.py --env-id " + problem + "-v2" + " --seed $SLURM_ARRAY_TASK_ID\n"
@@ -91,3 +90,24 @@ for algorithm in algorithms:
     # f3.close()
     # f2.seek(0)
 f.close()
+
+import glob
+from functools import reduce
+
+all_list = []
+for algorithm in algorithms:
+    all_files = glob.glob("../../hpc_scripts/" + algorithm + "/*.sl")
+    all_list.extend(list(map(lambda x: "".join(["sbatch ./",x.split("/")[-2], "/", x.split("/")[-1]]), all_files)))
+# new_list = reduce(lambda x,y:x.extend(y), all_list)
+command = reduce(lambda x,y: "".join([x, "\n", y]), all_list)
+with open("../../hpc_scripts/run.sh", "w") as f3:
+    command = reduce(lambda x,y: "".join([x, "\n", y]), all_list)
+    f3.write(command)
+
+# f3 = open(directory + "/run_grid_ex_" + algorithm + ".sh", 'w')
+# for line in f2:
+#     if "ACKTR" in line:
+#         line = line.replace("ACKTR", algorithm)
+#     f3.write(line)
+# f3.close()
+# f2.seek(0)
