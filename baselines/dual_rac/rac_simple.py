ad2628359ac8371ab58b102a808edce1a1781df2
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-10 15:22:15 +1300

    1. Decrease the rate 2. Update more

--- baselines/dual_rac/rac_simple.py
+++ baselines/dual_rac/rac_simple.py
@@ -269,7 +269,16 @@ def learn(env, test_env, policy_fn, *,
                     [scaling_factor[i] * pol_gradients[i] for i in range(len(scaling_factor))], axis = 0)
                 pol_adam.update(coef * sum_weighted_pol_gradients, optim_stepsize * 0.1 * cur_lrmult)
                 pol_gradients = []
-                t_0 = t
+                t_0 = max(t - update_step_threshold, 0)
+            elif t > update_step_threshold:
+                scaling_factor = [rho ** (t - i) for i in range(t_0, t)]
+                coef = update_step_threshold / np.sum(scaling_factor)
+                sum_weighted_pol_gradients = np.sum(
+                    [scaling_factor[i] * pol_gradients[i] for i in range(len(scaling_factor))], axis = 0)
+                pol_adam.update(coef * sum_weighted_pol_gradients, optim_stepsize * 0.1 * cur_lrmult)
+                pol_gradients = []
+                t_0 = max(t - update_step_threshold, 0)
+
             ob = next_ob
             if timesteps_so_far % 10000 == 0:
                 # result_record()
@@ -281,14 +290,14 @@ def learn(env, test_env, policy_fn, *,
                 rewbuffer.extend(rews)
                 result_record()
             if done:
-                if len(pol_gradients) > 0:
-                    scaling_factor = [rho ** (t - i) for i in range(t_0, t)]
-                    coef = (t - t_0) / np.sum(scaling_factor)
-                    sum_weighted_pol_gradients = np.sum(
-                        [scaling_factor[i] * pol_gradients[i] for i in range(len(scaling_factor))], axis = 0)
-                    pol_adam.update(coef * sum_weighted_pol_gradients, optim_stepsize * 0.1 * cur_lrmult)
-                    pol_gradients = []
-                    t_0 = t
+                # if len(pol_gradients) > 0:
+                #     scaling_factor = [rho ** (t - i) for i in range(t_0, t)]
+                #     coef = (t - t_0) / np.sum(scaling_factor)
+                #     sum_weighted_pol_gradients = np.sum(
+                #         [scaling_factor[i] * pol_gradients[i] for i in range(len(scaling_factor))], axis = 0)
+                #     pol_adam.update(coef * sum_weighted_pol_gradients, optim_stepsize * 0.1 * cur_lrmult)
+                #     pol_gradients = []
+                #     t_0 = t
                 print(
                     "Episode {} - Total reward = {}, Total Steps = {}".format(episodes_so_far, cur_ep_ret, cur_ep_len))
 
