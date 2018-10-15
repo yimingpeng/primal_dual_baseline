ce62ba051e50ff5ae8c1f63116a763c1fb1c2ca5
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-15 14:54:20 +1300

    1. Modify the policy back to bias with zero initializer

--- baselines/rac/mlp_policy.py
+++ baselines/rac/mlp_policy.py
@@ -20,35 +20,28 @@ class MlpPolicy(object):
         sequence_length = None
 
         ob = U.get_placeholder(name = "ob", dtype = tf.float32, shape = [sequence_length] + list(ob_space.shape))
-
+        # self.std = tf.Variable(1.0)
         self.std = tf.constant(1.0)
         with tf.variable_scope("obfilter"):
             self.ob_rms = RunningMeanStd(shape = ob_space.shape)
 
         with tf.variable_scope('vf'):
-            # beta = 0.9
-            # Add gaussian RBF
-            # centers = self.ob_rms.mean
-            # obz = tf.clip_by_value(tf.exp(-beta*tf.square(tf.norm(ob - self.ob_rms.mean))), -5.0, 5.0)
-
-            # obz = tf.clip_by_value(tf.exp(-beta*tf.reduce_sum(tf.multiply(ob, self.ob_rms.mean))), -5.0, 5.0)
-
             obz = tf.clip_by_value((ob - self.ob_rms.mean) / self.ob_rms.std, -5.0, 5.0)
             last_out = obz
             # for i in range(num_hid_layers):
-                # last_out = tf.nn.tanh(tf.layers.dense(last_out, hid_size, name = "fc%i" % (i + 1),
-                #                                       kernel_initializer = U.normc_initializer(1.0)))
-            self.vpred = tf.layers.dense(last_out, 1, name = 'final', kernel_initializer = U.normc_initializer(1.0)
-                                         , bias_initializer = None)[:,
+            #     last_out = tf.nn.tanh(tf.layers.dense(last_out, hid_size, name = "fc%i" % (i + 1),
+            #                                           kernel_initializer = U.normc_initializer(1.0)))
+            self.vpred = tf.layers.dense(last_out, 1, name = 'final', kernel_initializer = U.normc_initializer(1.0))[:,
                          0]
 
         with tf.variable_scope('pol'):
             last_out = obz
             # for i in range(num_hid_layers):
-            #     last_out = tf.nn.tanh(tf.layers.dense(last_out, hid_size, name = 'fc%i' % (i + 1), kernel_initializer = U.normc_initializer(1.0)))
+            #     last_out = tf.nn.tanh(tf.layers.dense(last_out, hid_size, name = 'fc%i' % (i + 1),
+            #                                           kernel_initializer = U.normc_initializer(1.0)))
             if gaussian_fixed_var and isinstance(ac_space, gym.spaces.Box):
                 mean = tf.layers.dense(last_out, pdtype.param_shape()[0] // 2, name = 'final',
-                                       kernel_initializer = U.normc_initializer(0.01), bias_initializer = None)
+                                       kernel_initializer = U.normc_initializer(0.01))
                 # logstd = tf.get_variable(name="logstd", shape=[1, pdtype.param_shape()[0]//2], initializer=tf.zeros_initializer())
                 # pdparam = tf.concat([mean, mean * 0.0 + tf.ones(pdtype.param_shape()[0])//2], axis = 1)
                 # logstd = tf.get_variable(name="logstd", shape=[1, pdtype.param_shape()[0]//2], initializer=tf.zeros_initializer())
@@ -69,7 +62,6 @@ class MlpPolicy(object):
 
     def act(self, stochastic, ob):
         ac1, vpred1 = self._act(stochastic, ob[None])
-        # ac1 = tf.clip_by_value(ac1, -1., 1.)
         return ac1[0], vpred1[0]
 
     def get_variables(self):
