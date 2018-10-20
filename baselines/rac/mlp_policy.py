8d8bc55444f3ec92755aaa90a4f24989724b7975
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-20 17:20:40 +1300

    1. Decay the std 2. Remove restrictions

--- baselines/rac/mlp_policy.py
+++ baselines/rac/mlp_policy.py
@@ -35,23 +35,23 @@ class MlpPolicy(object):
             #     feature_funcs.append(lambda s, i=i: tf.cos(i*np.pi*s))
             # obz = tf.concat([func(obz) for func in feature_funcs], axis = 1)
             last_out = obz
-            # for i in range(num_hid_layers):
-            #     last_out = tf.nn.tanh(tf.layers.dense(last_out, hid_size, name = "fc%i" % (i + 1),
-            #                                           kernel_initializer = U.normc_initializer(1.0)))
+            for i in range(num_hid_layers):
+                last_out = tf.nn.tanh(tf.layers.dense(last_out, hid_size, name = "fc%i" % (i + 1),
+                                                      kernel_initializer = U.normc_initializer(1.0)))
             self.vpred = tf.layers.dense(last_out, 1, name = 'final', kernel_initializer = U.normc_initializer(1.0))[:,
                          0]
 
         with tf.variable_scope('pol'):
             last_out = obz
-            # for i in range(num_hid_layers):
-            #     last_out = tf.nn.tanh(tf.layers.dense(last_out, hid_size, name = 'fc%i' % (i + 1), kernel_initializer = U.normc_initializer(1.0)))
+            for i in range(num_hid_layers):
+                last_out = tf.nn.tanh(tf.layers.dense(last_out, hid_size, name = 'fc%i' % (i + 1), kernel_initializer = U.normc_initializer(1.0)))
             if gaussian_fixed_var and isinstance(ac_space, gym.spaces.Box):
                 mean = tf.layers.dense(last_out, pdtype.param_shape()[0] // 2, name = 'final',
                                        kernel_initializer = U.normc_initializer(0.01))
                 # logstd = tf.get_variable(name="logstd", shape=[1, pdtype.param_shape()[0]//2], initializer=tf.zeros_initializer())
                 # pdparam = tf.concat([mean, mean * 0.0 + tf.ones(pdtype.param_shape()[0])//2], axis = 1)
-                # logstd = tf.get_variable(name="logstd", shape=[1, pdtype.param_shape()[0]//2], initializer=tf.zeros_initializer())
-                logstd = tf.multiply(tf.ones(shape=[1, pdtype.param_shape()[0]//2]), tf.constant(0.01))
+                logstd = tf.get_variable(name="logstd", shape=[1, pdtype.param_shape()[0]//2], initializer=tf.zeros_initializer())
+                # logstd = tf.multiply(tf.ones(shape=[1, pdtype.param_shape()[0]//2]), tf.constant(0.01))
                 pdparam = tf.concat([mean, mean * 0.0 + logstd], axis=1)
             else:
                 pdparam = tf.layers.dense(last_out, pdtype.param_shape()[0], name = 'final',
