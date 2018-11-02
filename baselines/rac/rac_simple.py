f3af3cea5ed060fe17a6e11c758730cba753e85f
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-11-02 15:01:19 +1300

    1. Clip actions for RAC test

--- baselines/rac/rac_simple.py
+++ baselines/rac/rac_simple.py
@@ -36,6 +36,7 @@ def traj_segment_generator(pi, env, horizon, stochastic):
     while True:
         prevac = ac
         ac, vpred = pi.act(stochastic, ob)
+        ac = np.clip(ac, env.action_space.low, env.action_space.high)
         # Slight weirdness here because we need value function at time T
         # before returning segment [0, T-1] so we get the correct
         # terminal value
