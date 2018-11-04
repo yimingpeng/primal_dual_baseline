1c5c4b0bd5376051ec0d02310f4ad9956f890947
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-11-04 14:59:59 +1300

    1. Adjusting DRAC

--- baselines/dual_rac/run_pybullet.py
+++ baselines/dual_rac/run_pybullet.py
@@ -9,7 +9,7 @@ sys.path.append(
             os.path.abspath(os.path.join(os.getcwd(), os.pardir)), os.pardir)))
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(os.path.dirname(currentdir))
-os.sys.path.insert(0,parentdir)
+os.sys.path.insert(0, parentdir)
 # very important, don't remove, otherwise pybullet cannot run (reasons are unknown)
 import pybullet_envs
 from baselines.common.cmd_util import pybullet_arg_parser, make_pybullet_env
@@ -19,30 +19,33 @@ from baselines import logger
 
 def train(env_id, num_timesteps, seed):
     from baselines.dual_rac import mlp_policy, rac_simple
-    U.make_session(num_cpu=1).__enter__()
+    U.make_session(num_cpu = 1).__enter__()
+
     def policy_fn(name, ob_space, ac_space):
-        return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
-            hid_size=64, num_hid_layers=2)
+        return mlp_policy.MlpPolicy(name = name, ob_space = ob_space, ac_space = ac_space,
+                                    hid_size = 64, num_hid_layers = 2)
 
     env = make_pybullet_env(env_id, seed)
     rac_simple.learn(env, policy_fn,
-            max_timesteps=num_timesteps,
-            timesteps_per_actorbatch=2048,
-            clip_param=0.2, entcoeff=0.0,
-            optim_epochs=1, optim_stepsize=3e-4, optim_batchsize=64,
-            gamma=0.99, lam=0.95,
-            rho = 0.95,  # Gradient weighting factor
-            update_step_threshold = 25, # Updating step threshold
+                     max_timesteps = num_timesteps,
+                     timesteps_per_actorbatch = 2048,
+                     clip_param = 0.2, entcoeff = 0.0,
+                     optim_epochs = 1, optim_stepsize = 3e-4, optim_batchsize = 64,
+                     gamma = 0.99, lam = 0.95,
+                     rho = 0.95,  # Gradient weighting factor
+                     update_step_threshold = 5,  # Updating step threshold
                      shift = 0,
-                     schedule='linear'
-        )
+                     schedule = 'linear'
+                     )
     env.close()
+
 
 def main():
     args = pybullet_arg_parser().parse_args()
-    logger.configure(format_strs=['stdout', 'log', 'csv'], log_suffix = "Dual_RAC-"+args.env)
-    logger.log("Algorithm: Dual_RAC-"+args.env)
-    train(args.env, num_timesteps=args.num_timesteps, seed=args.seed)
+    logger.configure(format_strs = ['stdout', 'log', 'csv'], log_suffix = "Dual_RAC-" + args.env)
+    logger.log("Algorithm: Dual_RAC-" + args.env)
+    train(args.env, num_timesteps = args.num_timesteps, seed = args.seed)
+
 
 if __name__ == '__main__':
     main()
