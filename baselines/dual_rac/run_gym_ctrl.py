afe7d7b4dcd9b4542b554e0bc1d67fd389aefbdc
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-11-12 15:14:16 +1300

    1. Adapt the parameters

--- baselines/dual_rac/run_gym_ctrl.py
+++ baselines/dual_rac/run_gym_ctrl.py
@@ -23,7 +23,7 @@ def train(env_id, num_timesteps, seed):
                      max_timesteps = num_timesteps,
                      timesteps_per_actorbatch = 2048,
                      clip_param = 0.2, entcoeff = 0.0,
-                     optim_epochs = 1, optim_stepsize = 0.1, optim_batchsize = 64,
+                     optim_epochs = 1, optim_stepsize = 3e-3, optim_batchsize = 64,
                      gamma = 0.99, lam = 0.95,
                      rho = 0.95,  # Gradient weighting factor
                      update_step_threshold = 20,  # Updating step threshold
