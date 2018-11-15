c64550c632154ede3ffea0335cc5aeb36dd6a31a
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-11-16 12:40:23 +1300

    Remove walker2D

--- baselines/common/generate_grid_scripts.py
+++ baselines/common/generate_grid_scripts.py
@@ -33,7 +33,7 @@ algorithms = ["ars", "dual_nac_advantage", "dual_nac_fisher",
 #                    "InvertedPendulum", "InvertedPendulumSwingup", "Reacher",
 #                    "Walker2D"]
 bullet_problems = ["InvertedDoublePendulum",
-                   "InvertedPendulum", "InvertedPendulumSwingup", "Walker2D"]
+                   "InvertedPendulum", "InvertedPendulumSwingup"]
 gym_problems = ["MountainCarContinuous", "LunarLanderContinuous", "BipedalWalker"]
 seeds = range(5)
 # Generate for Bullet problems
