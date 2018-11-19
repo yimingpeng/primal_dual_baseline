19a34d863ec60d9d0daa140ba2947ef250987a94
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-11-19 17:27:56 +1300

    Add single machine running bash

---
+++ single_scripts/nac_fisher/nac_fisher_LunarLanderContinuous.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+experimentName="baselines"
+pyName="run_gym_ctrl.py"cd $experimentName/ppo/
+
+for i in 1 2 3 4 5
+do
+python $pyName --env LunarLanderContinuous-v2 --seed $SGE_TASK_ID
+done
