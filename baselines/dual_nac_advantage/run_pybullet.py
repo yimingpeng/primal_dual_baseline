99dd3cff878740d7d55b70f9b1393ef0eca85d38
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-12-03 09:17:21 +1300

    1. Add features to dual nac advantage

--- baselines/dual_nac_advantage/run_pybullet.py
+++ baselines/dual_nac_advantage/run_pybullet.py
@@ -22,17 +22,17 @@ def train(env_id, num_timesteps, seed):
     U.make_session(num_cpu=1).__enter__()
     def policy_fn(name, ob_space, ac_space):
         return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
-            hid_size=64, num_hid_layers=2)
+            hid_size=32, num_hid_layers=2)
 
     env = make_pybullet_env(env_id, seed)
     nac_simple.learn(env, policy_fn,
                      max_timesteps = num_timesteps,
                      timesteps_per_actorbatch = 2048,
                      clip_param = 0.2, entcoeff = 0.0,
-                     optim_epochs = 1, optim_stepsize = 3e-6, optim_batchsize = 64,
+                     optim_epochs = 1, optim_stepsize = 3e-4, optim_batchsize = 64,
                      gamma = 0.99, lam = 0.95,
                      rho = 0.95,  # Gradient weighting factor
-                     update_step_threshold = 5,  # Updating step threshold
+                     update_step_threshold = 3,  # Updating step threshold
                      shift = 0,
                      schedule = 'linear'
         )
